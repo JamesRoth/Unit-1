b88b7f90b9b6c1ec208a4a7a39d365771423c8fb
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-17 13:59:17 -0500

    Updated from Brython Server: 1/17/2018 1:59:16 PM

--- nameAge.py
+++ nameAge.py
@@ -2,7 +2,8 @@
 #1/17/18
 #nameAge.py - splitting and characters in a string
 name=input("What is your first and last name? ")
-age=input("What is your age? ")
+age=int(input("What is your age? "))
 firstName,lastName=name.split()
 print("Your first name has ", len(firstName), "letters")
 print("Your last name has ", len(lastName), "letters")
+print("You will be", age+1, "next year")
