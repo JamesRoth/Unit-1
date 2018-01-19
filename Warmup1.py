85fd0378a1640ac48924652d3ed06bb7bcc17c60
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-19 08:24:25 -0500

    Updated from Brython Server: 1/19/2018 8:24:24 AM

--- Warmup1.py
+++ Warmup1.py
@@ -1,1 +1,6 @@
-
+#James Roth
+#1/19/18
+#Warmup1.py - warmup
+base=float(input("What is the base of your triangle?" ))
+height=float(input("What is the height of your triangle?" ))
+print("The area is", 1/2*base*height)
