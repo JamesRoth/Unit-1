d68e38f3857e68f5cb13f5c3000d022103204c4f
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-19 08:06:38 -0500

    Update tipCalculator.py

--- tipCalculator.py
+++ tipCalculator.py
@@ -1,7 +1,7 @@
 #James Roth
 #1/19/18
 #tipCalculator.py - how much to tip
-meal=int=input("How much was the meal? ")
-tip=int=input("How much would you like to tip? ")
+meal=float(input("How much was the meal? "))
+tip=float(input("How much would you like to tip? "))
 print("The tip is", 
 
