c188acec909377a6dca99fd58322fd7ca9ab54c1
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-19 08:09:05 -0500

    Update pythagoreanDemo.py

--- pythagoreanDemo.py
+++ pythagoreanDemo.py
@@ -1,1 +1,10 @@
 
+#James Roth
+#1/19/18
+#pythagoreanDemo.py - how to use import
+
+from math import sqrt
+
+a=float(input("Enter side A:"))
+b=float(input("Enter side B:"))
+print("The hypotenuse is", sqrt(a**2+b**2))
