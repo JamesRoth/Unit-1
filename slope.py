6c28904549665c5399d9f868d63804bde8311756
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-19 08:38:30 -0500

    Updated from Brython Server: 1/19/2018 8:38:29 AM

--- slope.py
+++ slope.py
@@ -1,1 +1,14 @@
+#James Roth
+#1/19/18
+#Slope - calculating slope
 
+x1=float(input("What is the first x coordinate?" ))
+x2=float(input("What is the second x coordinate?" ))
+y1=float(input("What is the first y coordinate?" ))
+y2=float(input("What is the second y coordinate?" ))
+
+slope=float(round((y1-y2)/(x1-x2), 3))
+print("The slope is", slope)
+
+b=float(y1-slope*x1)
+print("The equation of the line is y = ",slope,"x +", b)
