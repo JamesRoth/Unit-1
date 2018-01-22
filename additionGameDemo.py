375008eedad81b54daf5937292822aec6a1c3bd8
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-22 08:16:19 -0500

    Updated from Brython Server: 1/22/2018 8:16:17 AM

--- additionGameDemo.py
+++ additionGameDemo.py
@@ -6,6 +6,5 @@ from random import randint
 
 num1 = randint(-10, 10)
 num2 = randint(-10,10)
-print(num1, "+", num1)
-ans=int(input("= "))
-print(ans == num1+num2)
+ans=int(input(str(num1), "+", str(num1),"= "))
+print(ans == num1 + num2)
