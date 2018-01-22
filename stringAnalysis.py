8037fe57a3613523aa7eca576962e7c4f254ecac
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-22 08:32:58 -0500

    Updated from Brython Server: 1/22/2018 8:32:57 AM

--- stringAnalysis.py
+++ stringAnalysis.py
@@ -2,5 +2,9 @@
 #1/22/18
 #stringAnalysis.py - words and character counts
 
-sentence=input("Enter a sentence")
+sentence=input("Enter a sentence ")
 characters=len(sentence)
+letters=(characters-sentence.count(" "))
+findchar=input("Enter a character to search for ")
+
+print("Your sentence has", characters/(characters-sentence.count(" "), "words and", characters "characters and", letters "letters")
