7c010a2a78bea9b5b62fb080b80fae706165e3ae
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-24 08:39:04 -0500

    Updated from Brython Server: 1/24/2018 8:39:04 AM

--- change.py
+++ change.py
@@ -2,4 +2,11 @@
 #1/24/18
 #change.py - making change
 
-change=int(input("Input a number of cents"))
+change=int(input("Input a number of cents: "))
+print("Quaters: ", change//25)
+change=change%25
+print("Dimes: ", change//10)
+change=change%10
+print("Nickels: ", change//5)
+change=change%5
+print("Pennies"
