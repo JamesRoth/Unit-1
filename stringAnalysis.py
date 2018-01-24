ea2d8310945fab486a0bf596dd5e4db9a71a2cd2
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-24 08:20:02 -0500

    Updated from Brython Server: 1/24/2018 8:20:01 AM

--- stringAnalysis.py
+++ stringAnalysis.py
@@ -6,6 +6,7 @@ sentence=input("Enter a sentence: ")
 characters=int(len(sentence))
 letters=int((characters-sentence.count(" ")))
 findchar=input("Enter a character to search for: ")
+findword=input("Enter a word to search for: ")
 
 print("Your sentence has", (sentence.count(" "))+1, "words and", characters, "characters and", letters, "letters.")
-print("Your sentence has", sentence.count(findchar), "of the character", findchar + ".")
+print("Your sentence has", sentence.count(findchar), "of the character", findchar, "and", sentence.count(findword), "of the word", findword + ".")
