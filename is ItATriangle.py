c4e1a58255d550d863b645682d27da26774a295c
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-25 08:05:16 -0500

    Updated from Brython Server: 1/25/2018 8:05:15 AM

--- is ItATriangle.py
+++ is ItATriangle.py
@@ -1,1 +1,7 @@
+#James Roth
+#1/25/18
+#isItATriangle.py - sides of a triangle and can they make a triangle
 
+side1=int(input("Enter the length of side 1"))
+side2=int(input("Enter the length of side 2"))
+side3=int(input("Enter the length of side 3"))
