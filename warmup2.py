7ae636208940b69fae47c874e039e93c8ebc3b21
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-25 08:15:40 -0500

    Updated from Brython Server: 1/25/2018 8:15:39 AM

--- warmup2.py
+++ warmup2.py
@@ -1,1 +1,7 @@
+#James Roth
+#1/25/18
+#warmup2.py - random integers and names
 
+from random import randint
+name=input("What is your name?: ")
+print(name, ", your lucky number is", randint(-100,100))
