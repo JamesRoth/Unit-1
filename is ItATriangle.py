435329ce403352b4910c4cddbef07c55fe5bc7a0
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-25 08:25:18 -0500

    Updated from Brython Server: 1/25/2018 8:25:17 AM

--- is ItATriangle.py
+++ is ItATriangle.py
@@ -2,7 +2,7 @@
 #1/25/18
 #isItATriangle.py - sides of a triangle and can they make a triangle
 
-side1=float(input("Enter the length of side 1"))
-side2=float(input("Enter the length of side 2"))
-side3=float(input("Enter the length of side 3"))
-print( max(side1,side2,side3)<(min(side1,side2,side3)+
+side1=float(input("Enter the length of side 1: "))
+side2=float(input("Enter the length of side 2: "))
+side3=float(input("Enter the length of side 3: "))
+print( max(side1,side2,side3)<(side1+side2+side3)-(max(side1,side2,side3)) )
