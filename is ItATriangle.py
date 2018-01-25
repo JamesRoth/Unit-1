6090629d1b5b0a7bf0a38d8466b0f8b9f968b84c
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-01-25 08:23:57 -0500

    Updated from Brython Server: 1/25/2018 8:23:57 AM

--- is ItATriangle.py
+++ is ItATriangle.py
@@ -5,4 +5,4 @@
 side1=float(input("Enter the length of side 1"))
 side2=float(input("Enter the length of side 2"))
 side3=float(input("Enter the length of side 3"))
-
+print( max(side1,side2,side3)<(min(side1,side2,side3)+
